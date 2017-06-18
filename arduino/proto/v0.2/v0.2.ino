706eb9b72569cc825bc83640d2a43a4290fe46c2
John Wass <jwass3@gmail.com>
2017-06-18 10:53:33 -0400

    tracking completion

--- arduino/proto/v0.2/v0.2.ino
+++ arduino/proto/v0.2/v0.2.ino
@@ -1,4 +1,5 @@
 const int buad_rate = 9600;
+const int minr = 100;
 
 const byte
 s0 = A0, t0 = 9,
@@ -6,8 +7,10 @@ s1 = A1, t1 = 8,
 s2 = A2, t2 = 7,
 s3 = A3, t3 = 6;
 
-const byte ss[] = {s0, s1, s2, s3};
-const byte ts[] = {t0, t1, t2, t3};
+const byte sz = 4;
+const byte ss[sz] = {s0, s1, s2, s3};
+const byte ts[sz] = {t0, t1, t2, t3};
+byte gs[sz] = {0, 0, 0, 0};
 
 void setup() {
   for (auto t : ts) {
@@ -19,9 +22,22 @@ void setup() {
 
 void reset() {
   for (auto t : ts) digitalWrite(t, LOW);
+  for (int i = 0; i < sz; ++i) gs[i] = 1;
+}
+
+int sumgs() {
+  int sum = 0;
+  for (int i = 0; i < sz; ++i) sum += gs[i];
+  return sum;
 }
 
 void loop() {
+  if (!sumgs()) reset();
+
+  for (int i = 0; i < sz; ++i)
+    if (gs[i] && analogRead(ss[i]) > minr)
+      digitalWrite(ts[i], HIGH);
+
   if (Serial.available() > 0) {
     byte ser = Serial.read();
     switch (ser) {
